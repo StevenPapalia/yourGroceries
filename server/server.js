eb3163fa9c212a53d55f30a2aa7dd3bcd08dc533
Steven Papalia <stevenapapalia@yahoo.com>
2020-04-18 15:43:50 -0700

    Stripe Works!!!

--- server/server.js
+++ server/server.js
@@ -1,17 +1,46 @@
 const express = require("express");
+const stripeLoader = require("stripe");
 const path = require("path");
+const bodyParser = require("body-parser");
 const { connection } = require('../database/config');
+const  { stripeApiSecretKey } = require('./keys');
 
 const app = express();
 const port = 3000;
 
+const jsonParser = bodyParser.json();
+
 app.use(express.static(path.resolve(__dirname, '../dist/')));
+app.use(express.json());
 
 app.get('/groceries', (req, res) => {
   connection.query("SELECT * FROM groceries", (error, results) => {
     if (error) { res.sendStatus(500); throw error; }
     else res.send(results);
   });
+});
+
+const stripe = new stripeLoader(stripeApiSecretKey);
+
+const charge = (token, amt) => {
+  return stripe.charges.create({
+    amount: amt * 100,
+    currency: 'usd',
+    source: token,
+    description: 'grocery purchase', 
+  });
+}
+
+app.post('/purchase', jsonParser, async (req, res, next) => {
+  // console.log(req.body.token);
+  try {
+    let data = await charge(req.body.token.id, req.body.amount);
+    console.log(data);
+    res.send("Charged");
+  } catch(err) {
+    console.log(err);
+    res.status(500);
+  }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/PaymentForm.tsx
+++ src/PaymentForm.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { CardElement, injectStripe, ReactStripeElements } from 'react-stripe-elements';
+import { ajax } from 'jquery';
+import { CardElement, injectStripe, ReactStripeElements, CardNumberElement, CardExpiryElement, CardCVCElement } from 'react-stripe-elements';
 
 interface Props {
   stripe?: ReactStripeElements.StripeProps
@@ -13,18 +14,26 @@ const PaymentForm: React.FC<Props> = (props) => {
   const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => { 
     e.preventDefault();
     try {
-      let token = await props.stripe.createToken({ name, });
-      console.log(token);
-    } catch(e) { throw e; }
+      let { token } = await props.stripe.createToken({ name, });
+      let amount = Number(props.cart.reduce((total, el) => { return Number(total) + (el[0].price * el[1]); }, 0)).toFixed(2);
+      ajax({
+        method: 'POST',
+        url: '/purchase',
+        data: JSON.stringify({ token, amount }),
+        contentType: 'application/json',
+        success: (data) => { console.log(data) },
+        error: (err) => { console.log(err); },
+      });
+    } catch(err) { throw err; }
   }
 
   return (
     <div>
       <h1>Payment Form</h1>
       <form onSubmit={handleSubmit}>
-        <label>Name:</label><input type="text" value={name} onChange={updateName}/>
-        <label>Amount:</label><span>${Number(props.cart.reduce((total, el) => { return Number(total) + (el[0].price * el[1]); }, 0)).toFixed(2)}</span>
-        <label>Credit Card Information</label><CardElement />
+        <div><label>Name: </label><input type="text" value={name} onChange={updateName}/></div>
+        <div><label>Amount: </label><span>${Number(props.cart.reduce((total, el) => { return Number(total) + (el[0].price * el[1]); }, 0)).toFixed(2)}</span></div>
+        <div><label>Credit Card Information: </label><CardElement /></div>
         <button>Charge!</button>
       </form>
     </div>
