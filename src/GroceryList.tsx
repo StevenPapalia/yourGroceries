0699b97016ea88f790fc227a1df0798b761a1a2a
Steven Papalia <stevenapapalia@yahoo.com>
2020-04-19 17:05:38 -0700

    Add search functionality

--- src/GroceryList.tsx
+++ src/GroceryList.tsx
@@ -8,20 +8,24 @@ interface Props {
 
 const GroceryList: React.FC<Props> = (props) => {
   const [selected, setSelected] = React.useState<string>("All");
+  const [searchForm, setSearchForm] = React.useState<string>("");
+
+  const updateSearchForm = (e) => { setSearchForm(e.target.value); e.preventDefault(); }
 
   const getSelected = (e) => { setSelected(e.target.value); e.preventDefault(); }
   
   return (
     <div>
-      <span>Sort By:</span>
+      <span>Sort By: </span>
       <select value={selected} onChange={getSelected}>
         <option value="All">All</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
         <option value="Meat">Meat</option>
       </select>
+      <span>Search: <input type="text" value={searchForm} onChange={updateSearchForm}/></span>
       <div>
-        {props.groceries.map((grocery, index) => {
+        {props.groceries.filter((grocery) => { return grocery.item.toUpperCase().includes(searchForm.toUpperCase())}).map((grocery, index) => {
           return selected === "All" ? <GroceryItem key={index} grocery={grocery} addToCart={props.addToCart} />
           : selected === grocery.category ? <GroceryItem key={index} grocery={grocery} addToCart={props.addToCart} />
           : null
